1db005b227caf52449223608de52487c1d8df600
kmng1 <kmng@ieee.org>
2018-03-03 11:45:28 +0800

    Lesson 49 - test cases for Inbox

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,7 +1,12 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+// workaround for web3 1.0.0-beata.26 <-----------------
+// const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -14,13 +19,27 @@ beforeEach(async () => {
   // use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy( {data: bytecode, arguments: ['Hi there!']} )
-    .send({ from: accounts[0], gas: '1000000' })
-  console.log();
+    .send({ from: accounts[0], gas: '1000000' });
+
+// workaround for web3 1.0.0-beata.26 <-----------------
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
-  it('deploy a contract', () => {
-    console.log(inbox);
+  it('deploys a contract', () => {
+    // console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 
 });
