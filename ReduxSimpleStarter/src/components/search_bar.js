6badbd043bc2e6dcef9647a608731204762c5a14
kmng1 <kmng@alumni.nus.edu.sg>
2018-03-16 17:01:12 +0800

    React Tutorial Lesson 240 - styling

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
     this.state = { term: ''};
   }
 
   render() {
     return (
-      <div>
-        <input onChange={(event) => this.setState( { term: event.target.value })}/>;
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={(event) => this.setState( { term: event.target.value })}/>;
       </div>
 
     )
