24194a3256e95c1bee834b5c6c34383ab94bf7e6
kmng1 <kmng@alumni.nus.edu.sg>
2018-03-16 12:49:29 +0800

    React Tutorial Lesson 221 - Setup

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+
+// import App from './components/app';
+// import reducers from './reducers';
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const App = () => {
+  return <div>Hi!</div>;
+};
+
+ReactDOM.render(
+    <App />
+  , document.querySelector('.container'));
+
+/*
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
+*/
