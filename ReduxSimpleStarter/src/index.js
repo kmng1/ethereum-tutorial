77b99b1c6a4488b86aaad3d91eff13a13caf0ae2
kmng1 <kmng@alumni.nus.edu.sg>
2018-03-16 16:29:37 +0800

    React Tutorial Lesson 238 - video details

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -11,6 +11,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyB24sSvUB_J6f9TkqIfCkhBagYqlxjGKlk';
 
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
