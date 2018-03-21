580ee3fb10ac8709437d14f0e1ff7f4cb1324094
kmng1 <kmng@alumni.nus.edu.sg>
2018-03-21 17:16:42 +0800

    Sec 7 Lesson 184 - Show campaign details

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,8 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xaF4f93De413099F22c93a03f04D1a024A82a3937'
+  '0xc6640b314C05647b01f2c25d529081e92E6C9429'
+  // '0xaF4f93De413099F22c93a03f04D1a024A82a3937'
 );
 
 export default instance;
